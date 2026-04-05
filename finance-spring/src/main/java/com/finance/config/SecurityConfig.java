1e8abe0560fcca3b01446888841041b46a789235
AISHWARYA <96626999+Aishwarya8py@users.noreply.github.com>
2026-04-06 00:44:44 +0530

    Update SecurityConfig.java

--- finance-spring/src/main/java/com/finance/config/SecurityConfig.java
+++ finance-spring/src/main/java/com/finance/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .cors(Customizer.withDefaults())
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
