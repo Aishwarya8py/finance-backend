cd0993d679369b71e76fe7dfaf8d6a062f1a40fb
AISHWARYA <96626999+Aishwarya8py@users.noreply.github.com>
2026-04-06 00:02:27 +0530

    Update OpenApiConfig.java

--- finance-spring/src/main/java/com/finance/config/OpenApiConfig.java
+++ finance-spring/src/main/java/com/finance/config/OpenApiConfig.java
@@ -1,15 +1,12 @@
 package com.finance.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -17,10 +14,6 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .servers(List.of(
-                        new Server()
-                                .url("https://finance-backend-production-5c2e.up.railway.app")
-                ))
                 .info(new Info()
                         .title("Finance Backend API")
                         .version("1.0.0")
